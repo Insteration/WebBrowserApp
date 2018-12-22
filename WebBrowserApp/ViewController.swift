ff6cc663794934aeaa5cb5dea416d935e70ae140
Artem Karmaz <inste@icloud.com>
2018-12-22 20:39:52 +0200

    Initial Commit

---
+++ WebBrowserApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WebBrowserApp
+//
+//  Created by Artem Karmaz on 12/22/18.
+//  Copyright © 2018 Artem Karmaz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
